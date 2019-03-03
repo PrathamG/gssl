739b00bce1d39fa2103f04a45138ccde47f82575
Pratham <goradiapratham@gmail.com>
2019-03-03 16:46:55 +0800

    Navbar and home page development

---
+++ index.php
@@ -0,0 +1,48 @@
+<html>
+<head>
+	<?php include "inc_head.php"; ?>
+</head>
+<body>
+	<?php include "navbar.php" ?>
+	<div class = "container-fluid">
+		<div class = "home-panel">
+			<div class = "main-text">
+				<span>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam
+				</span>
+				<div class = "main-button" style = "margin-right: 35px">
+					Our Products
+				</div>
+				<div class = "main-button">
+					Make Enquiry
+				</div>
+			</div>
+		</div>
+		<div class = "wlc-panel">
+			<div class = "row panel-header">
+				Welcome to Goradia Special Steels Ltd.
+			</div>
+			<div class = "row panel-deet">
+				We are a specialty steel producer in India with established infrastructure for manufacturing, testing and delivering expert quality steel. We proudly serve over 500 customers across the world. Our extensive product range includes Tool Steels, Die Steels, Alloy Steels, Stainless Steels, Plastic Mould Steels, Carbon Steels, Mild Steels, Structural Steel and Steel Plates, available in various grades, shapes and sizes. 
+			</div>
+			<div class = "row panel-link">
+				<a><span>View our entire range of products <i class="fas fa-arrow-right"></i></span></a>
+			</div>
+		</div>
+		<div class = "prd-panel">
+			<div style = "width: 60%; height: 100%; padding: 0 20px 0 25px; background-color: rgba(0, 0, 0, 0.6)">
+				<div class = "prd-header">
+					Exceptional Infrastructure For Exceptional Steel
+				</div>
+				<div class = "prd-deet">
+					Our comprehensive and modern infrastructure set up allows us to take complete care of your steel needs. Our manufacturing plant is enabled to take complete responsibility from manufacturing, testing to treatment. Our established storage and transport infrastructure allows us to deliver your steel on time, every time. In addition, our specially designed, innovative manufacturing process guarantees finest quality, micro-clean steel developed exactly to your specifications. 
+				</div>
+				<div class = "prd-link">
+					Learn more abot our innovation and infrastructure <i class="fas fa-arrow-right"></i>
+				</div>
+			</div>
+		</div>
+	</div>
+	<?php include "inc_script.php"; ?>
+</body>
+</html>
---
+++ navbar.php
@@ -0,0 +1,11 @@
+<div class = "masthead">
+	<img src = "logo.png">
+	<ul>
+		<li><a>Home</a></li>
+		<li><a>About Us</a></li>
+		<li><a>Products</a></li>
+		<li><a>Infrastructure</a></li>
+		<li><a>Certification</a></li>
+		<li><a>Contact Us</a></li>
+	</ul>
+</div>
